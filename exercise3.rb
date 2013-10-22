61e7f4c71da0eff410aa7817ae0ab325ff4ffc49
elmatematic0 <wkdalebout+github@gmail.com>
2013-10-22 17:19:27 -0400

    including statement requesting age, which returns a statement with birth year.

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What's your name?"
 name = gets.chomp
-puts "Hello, #{name}!"
+puts "How old are you?"
+age = gets.chomp.to_i
+birthyear = 2013 - age
+puts "Hello #{name}! It looks like you were born in the year of #{birthyear} if your birth month was before October. Otherwise your birth year is #{birthyear - 1}"
