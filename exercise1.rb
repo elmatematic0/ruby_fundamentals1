0f44eb46a7df59432970150dffd3e50a715cbc7f
elmatematic0 <wkdalebout+github@gmail.com>
2013-10-22 15:41:54 -0400

    covering string interpolation

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,4 @@
+=begin
 puts 2 
 puts 3
 puts 2 != 3
@@ -7,5 +8,17 @@ puts 'Hello World'
 
 puts 'Betty\'s pie shop'
 puts "Betty's pie shop"
+
+puts "Enter name"
+name = gets.chomp
+puts "Your name is #{name}"
+
+=end
+
+a = 20
+b = 65
+puts "The value of a is #{a}"
+puts "The value of b is #{b}"
+puts "The value of a + b is #{a + b}"
 
 
