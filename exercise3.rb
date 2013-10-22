8c9a88fc85cedc69e1fd0c2f4c807b2cb13a9f0f
elmatematic0 <wkdalebout+github@gmail.com>
2013-10-22 18:10:56 -0400

    used Time.year.now which replaces hardcoded 2013

--- exercise3.rb
+++ exercise3.rb
@@ -2,5 +2,5 @@ puts "What's your name?"
 name = gets.chomp
 puts "How old are you?"
 age = gets.chomp.to_i
-birthyear = 2013 - age
+birthyear = Time.now.year - age
 puts "Hello #{name}! It looks like you were born in the year of #{birthyear} if your birth month was before October. Otherwise your birth year is #{birthyear - 1}"
