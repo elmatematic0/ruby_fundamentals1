f18dc9b40c6a5541ced91e52a7af54310731291e
elmatematic0 <wkdalebout+github@gmail.com>
2013-10-22 17:56:55 -0400

    adding my fizzbuzz exercise.

---
+++ exercise4.rb
@@ -0,0 +1,17 @@
+counter = 1
+
+until counter == 101 
+if counter % 3 == 0 && counter % 5 == 0
+  puts "Bitmaker"
+elsif counter % 3 == 0
+  puts "Bit"
+elsif counter % 5 == 0
+  puts "Maker"
+end
+counter += 1
+end
+
+
+
+
+
